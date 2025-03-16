0b03569ad46d96b2272bbb08707651f0713ab7a1
michelle2662 <wkwan1122@gmail.com>
2025-03-16 09:13:58 -0700

    Finished Part 3

--- 8.5/Pokemon/app.js
+++ 8.5/Pokemon/app.js
@@ -3,7 +3,7 @@ let numbers = new Array(3);
 
 function getAllPokemon(){
 
-    return fetch("https://pokeapi.co/api/v2/pokemon/?limit=1302&offset=0")
+    return fetch("https://pokeapi.co/api/v2/pokemon/?limit=&offset=0")
     .then(response => {return response.json()})
     .then(data => {
         console.log(data)
@@ -45,6 +45,13 @@ function getPokemonSpecies(name){
     .catch(error => console.error("Species does not exist for pokemon"))
 }
 
+function getEnglishFlavorText(speciesData){
+    const entry = speciesData.flavor_text_entries.find(
+        entry => entry.language.name === 'en'
+    );
+    return entry ? entry.flavor_text.replace(/\f|\n/g, " ").trim() : "No English flavor text available.";
+}
+
 let pokemon1 = ""
 let pokemon2 = ""
 let pokemon3 = ""
@@ -55,10 +62,20 @@ getThreePokemon().then(pokemonData => {
     pokemon2 = pokemonData[1].name;
     pokemon3 = pokemonData[2].name;
 
-    let pokemons = [pokemon1, pokemon2, pokemon3];
-    for (let i = 0 ; i < pokemons.length; i ++) {
-        getPokemonSpecies(pokemons[i])
-    }
+    pokemons = [pokemon1,pokemon2,pokemon3 ]
+    let allPokemonPromsie = [getPokemonSpecies(pokemon1), getPokemonSpecies(pokemon2), getPokemonSpecies(pokemon3)];
+    Promise.all(allPokemonPromsie)
+    .then(results => {
+        const flavorTexts = results.map((speciesData, index) => {
+            const flavorText = getEnglishFlavorText(speciesData);
+            console.log(`${pokemons[index]}:`, flavorText);
+            return flavorText;
+        
+    })
+    console.log("All Flavor Texts:", flavorTexts);
 
 
-});
+})
+
+
+})
