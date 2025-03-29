7a4c31bbf768696b05eb4ff44d0709b94b632991
michelle2662 <wkwan1122@gmail.com>
2025-03-29 07:16:28 -0700

    updates

--- 12.2/prop_exercise/InventoryItem.js
+++ 12.2/prop_exercise/InventoryItem.js
@@ -1,12 +1,12 @@
-function InventoryItem ({name, type, quantity = 1, price = 0.0 })
-{
+	function InventoryItem ({name, type, quantity = 1, price = 0.0 })
+	{
 	return (
-		<div>
-			<h2> {name} ({type})</h2>
-			{quantity < 5 ? <Message> ⚠️ Low stock - consider restocking soon! </Message>  : null}
-			{quantity*price > 1000 ? <Message> 💰 High value - consider extra protection!</Message> : null}
-		</div>
+	<div>
+		<h2> {name} ({type})</h2>
+		{quantity < 5 ? <Message> ⚠️ Low stock - consider restocking soon! </Message>  : null}
+		{quantity*price > 1000 ? <Message> 💰 High value - consider extra protection!</Message> : null}
+	</div>
 	);
-}
+	}
 
 
