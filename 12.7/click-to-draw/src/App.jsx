da31a84feb48083d0a468516df5bba5a0a8bc4de
michelle2662 <wkwan1122@gmail.com>
2025-04-01 21:03:30 -0700

    Finished part 2

--- 12.7/click-to-draw/src/App.jsx
+++ 12.7/click-to-draw/src/App.jsx
@@ -6,7 +6,8 @@ function App() {
 
   const [deckId, setDeckId] = useState(null)
   const [cardImage, setCardImage] = useState(null)
-  const [remainingCards , setRemainingCards] = useState(52);
+  const [remainingCards , setRemainingCards] = useState(52)
+  const [shuffling, setIsShuffling] = useState(false)
 
   useEffect(() => {
     const getDeck = async () => {
@@ -38,15 +39,30 @@ function App() {
     }
   };
 
+  const shuffleCard = async() => {
+
+    setIsShuffling(true); 
+    try {
+      const response = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`);
+      setRemainingCards(52);  
+      setCardImage(null);  
+    } catch (error) {
+      alert("Error shuffling the deck");
+    }
+    setIsShuffling(false); 
+    
+  }
 
   return (
     <div className="App">
       <h1>Deck of Cards</h1>
       <div className="card-container">
         {cardImage && <img src={cardImage} />}
-        <button onClick={drawCard}>
+        <button onClick={drawCard} className = "draw-card">
           Draw a Card
         </button>
+        <button onClick={shuffleCard} disabled={shuffling}> Shuffle </button>
+
       </div>
       <p className = "remaining">Cards remaining: {remainingCards}</p>
     </div>
